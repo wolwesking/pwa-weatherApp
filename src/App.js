09253be111035abe9921ba473b1fe8bc9393ff2e
wolwesking <83821890+wolwesking@users.noreply.github.com>
2023-01-15 17:59:39 +0500

    beta0.1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React, { Component } from 'react'
+import Inside from './images/InsideFrame.png'
+import Outside from './images/OutsideFrame.svg'
+import styles from './page.module.css'
+import Timer from './images/Timer.svg'
+import Humadity from './images/Humadity.svg'
+import Temp from './images/Temp.svg'
+import Wind from './images/Wind.svg'
+
 
 export default class App extends Component {
   constructor(props) {
@@ -8,34 +16,79 @@ export default class App extends Component {
       fl: null,
       hu: null,
       tp: null,
-      ws: null
+      ws: null,
+      clockText: null,
     }
   }
 
-  componentDidMount() {
-    let { fl, hu, tp, ws } = this.state;
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Atyrau,kz&APPID=bd32786b7240ee712e3181243e5039ab")
+  async componentDidMount() {
+    await fetch("https://api.openweathermap.org/data/2.5/weather?q=Atyrau,kz&APPID=bd32786b7240ee712e3181243e5039ab")
       .then(res => res.json())
-      .then(res => {
-        fl = res.main.feels_like;
-        hu = res.main.humidity;
-        tp = res.main.temp;
-        ws = res.wind.speed;
-        
+      .then(data => {
+
+
         this.setState({
-          fl: parseInt(fl - 273.15),
-          hu: parseInt(hu),
-          tp: parseInt(tp - 273.15),
-          ws: parseInt(ws * 3.6),
+          fl: parseInt(data.main.feels_like - 273.15),
+          hu: parseInt(data.main.humidity),
+          tp: parseInt(data.main.temp - 273.15),
+          ws: parseInt(data.wind.speed * 3.6),
         })
       })
 
+    let date = new Date();
+    let day = date.getDay();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+
+    if (day === 0 || day === 6) {
+      this.setState({
+       clockText: "End"
+      })
+    } else{
+      if (hour === 9 && minute <= 49) {
+        this.setState ({
+          clockText: "10:10"
+        })
+      }
+      else if (hour === 12 && minute <= 30) {
+        this.setState({
+          clockText: "1:17"
+        })
+      }
+      else if (hour === 11 && minute <= 46) {
+        this.setState({
+          clockText: "12:30"
+        })
+      }
+      else {
+        this.setState({
+          clockText: "Bye"
+        })
+      }
+    }
+
   }
   render() {
-    let { fl, hu, tp, ws } = this.state;
+    let { fl, hu, tp, ws, clockText } = this.state;
+    let isInside = (fl < -16) ? true : false;
+
+
     return (
-      <div>
+      <div className={isInside ? styles.insideDiv : styles.outsideDiv}>
+        <img alt="Frame" className={isInside ? styles.insideImg : styles.outsideImg} src={isInside ? Inside : Outside}></img>
+
+        <h1 className={styles.tempH1}>{fl}°C</h1>
+        <div className={styles.clockDiv}>
+          <img alt="Timer" className={styles.timer} src={Timer} />
+          <span className={styles.time}>{clockText}</span>
+        </div>
+        <div className={styles.weatherDiv}>
+          <div><span>{hu}</span> <img alt='Humadity' src={Humadity} /></div>
+          <div><span>{tp}</span> <img alt='Temp' src={Temp} /></div>
+          <div><span>{ws}km/h</span> <img alt='Wind' src={Wind} /></div>
+        </div>
       </div>
     )
+
   }
 }
